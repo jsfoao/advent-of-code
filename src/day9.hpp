c70487c2bfbe06e4cb8214c06ee210b8a8c0224c
João Freire <jsfoao@gmail.com>
2022-12-09 15:08:59 +0100

    day9p2

--- src/day9.hpp
+++ src/day9.hpp
@@ -5,38 +5,174 @@
 #include <fstream>
 #include "utils/vec2.h"
 
+#define GRID_X 25
+#define GRID_Y 25
+
 namespace challenges
 {
-	Vector2 Step(Vector2 dir, Vector2& head, Vector2& tail)
+	struct Input
 	{
-		head += dir;
+		Vector2 Direction;
+		int Steps;
 
-		Vector2 tailDir;
-		Vector2 diff = head - tail;
-		if (diff.x == 0)
+		Input()
 		{
-			tailDir.x = 0;
+			Direction = Vector2::zero;
+			Steps = 0;
 		}
-		else
+		Input(Vector2 dir, int steps)
 		{
-			tailDir.x = diff.x / std::abs(diff.x);
-		}
-		if (diff.y == 0)
-		{
-			tailDir.y = 0;
-		}
-		else
-		{
-			tailDir.y = diff.y / std::abs(diff.y);
+			Direction = dir;
+			Steps = steps;
 		}
 
-		Vector2 goal = tail + tailDir;
-		if (goal != head)
+		static Input ProcessInput(std::string str)
 		{
-			tail += tailDir;
+			Input input;
+			switch (str[0])
+			{
+			case 'R':
+				input.Direction = Vector2::right;
+				break;
+			case 'L':
+				input.Direction = -Vector2::right;
+				break;
+			case 'U':
+				input.Direction = Vector2::up;
+				break;
+			case 'D':
+				input.Direction = -Vector2::up;
+				break;
+			default:
+				break;
+			}
+			std::string num = "";
+			for (int i = 2; i < str.size(); i++)
+			{
+				num += str[i];
+			}
+			input.Steps = std::stoi(num);
+			return input;
+		}
+	};
+
+	struct Knot
+	{
+		static std::vector<Knot*> Knots;
+
+		Knot* Ahead;
+		Vector2 Position;
+		std::vector<Vector2> Visited;
+		bool IsHead;
+
+		Knot(Knot* ahead, Vector2 pos)
+		{
+			Ahead = ahead;
+			Position = pos;
+			Knots.push_back(this);
+			IsHead = ahead == nullptr;
 		}
 
-		return tail;
+		Knot(Knot* ahead)
+		{
+			Ahead = ahead;
+			Position = Vector2::zero;
+			Knots.push_back(this);
+			IsHead = ahead == nullptr;
+		}
+
+		void Step()
+		{
+			// step in direction
+			Vector2 dir;
+			Vector2 diff = Ahead->Position - Position;
+			if (diff.x == 0)
+			{
+				dir.x = 0;
+			}
+			else
+			{
+				dir.x = diff.x / std::abs(diff.x);
+			}
+			if (diff.y == 0)
+			{
+				dir.y = 0;
+			}
+			else
+			{
+				dir.y = diff.y / std::abs(diff.y);
+			}
+
+			Vector2 final = Position + dir;
+			if (final != Ahead->Position)
+			{
+				Position += dir;
+			}
+
+			// add to unique visited
+			if (Visited.size() == 0)
+			{
+				Visited.push_back(Position);
+			}
+			else
+			{
+				bool found = false;
+				for (Vector2 v : Visited)
+				{
+					found = Position == v;
+					if (found)
+						break;
+				}
+				if (!found)
+					Visited.push_back(Position);
+			}
+		}
+
+		int GetVisitedCount()
+		{
+			return Visited.size();
+		}
+
+		static Knot* GetHead()
+		{
+			return Knots.front();
+		}
+
+		static Knot* GetTail()
+		{
+			return Knots.back();
+		}
+	};
+
+	std::vector<Knot*> Knot::Knots;
+
+	void DrawGrid()
+	{
+		system("CLS");
+		for (int y = GRID_Y; y >= -GRID_Y; y--)
+		{
+			for (int x = -GRID_X; x < GRID_X; x++)
+			{
+				std::string c = ".";
+				for (int i = 0; i < Knot::Knots.size(); i++)
+				{
+					if (Knot::Knots[i]->Position != Vector2(x, y))
+					{
+						continue;
+					}
+
+					if (Knot::Knots[i]->IsHead)
+					{
+						c = "H";
+						break;
+					}
+					c = std::to_string(i);
+					break;
+				}
+				std::cout << c;
+			}
+			std::cout << std::endl;
+		}
 	}
 
 	void day9_p1()
@@ -46,67 +182,35 @@ namespace challenges
 		if (!file.is_open())
 			return;
 
-		Vector2 head = Vector2::zero;
-		Vector2 tail = Vector2::zero;
-		std::vector<Vector2> visited;
+		const unsigned int totalKnots = 10;
+
+		new Knot(nullptr);
+		for (int i = 1; i < totalKnots; i++)
+		{
+			new Knot(Knot::Knots[i - 1]);
+		}
 
 		while (std::getline(file, line))
 		{
-			// process line
-			Vector2 dir;
-			switch (line[0])
+			Input input = Input::ProcessInput(line);
+			while (input.Steps > 0)
 			{
-			case 'R':
-				dir = Vector2::right;
-				break;
-			case 'L':
-				dir = -Vector2::right;
-				break;
-			case 'U':
-				dir = Vector2::up;
-				break;
-			case 'D':
-				dir = -Vector2::up;
-				break;
-			default:
-				break;
-			}
-			std::string num = "";
-			for (int i = 2; i < line.size(); i++)
-			{
-				num += line[i];
-			}
-			Vector2 visit;
-			unsigned int steps = std::stoi(num);
-			for (unsigned int i = 0; i < steps; i++)
-			{
-				visit = Step(dir, head, tail);
-
-				if (visited.size() == 0)
+				for (Knot* knot : Knot::Knots)
 				{
-					visited.push_back(visit);
-				}
-				else
-				{
-					bool found = false;
-					for (Vector2 v : visited)
+					if (knot->IsHead)
 					{
-						found = visit == v;
-						if (visit == v)
-						{
-							found = true;
-							break;
-						}
+						knot->Position += input.Direction;
 					}
-					if (!found)
+					else
 					{
-						visited.push_back(visit);
+						knot->Step();
 					}
 				}
+				input.Steps--;
 			}
 		}
 		file.close();
 
-		std::cout << visited.size() << std::endl;
+		std::cout << Knot::GetTail()->GetVisitedCount() << std::endl;
 	}
 }
